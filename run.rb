0d212c3ebf5a582d07b7db2c95354fc3456e2a02
Jeff Koenig <jkoenig@thinkthroughmath.com>
2015-07-13 12:34:59 -0400

    Spiking out basic functionality

--- run.rb
+++ run.rb
@@ -1,17 +1,67 @@
-#!/bin/env ruby
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'httparty'
+require 'diffy'
+
+ENDPOINTS_FILE='endpoints.txt'
+PROPERTIES_FILE='test.yaml'
+
+def read_file file
+  File.readlines(file)
+    .map {|r| r.strip }
+    .reject {|r| r.empty?}
+    .reject {|r| r.start_with?('#')}
+end
+
+def read_properties
+  YAML.load(File.read(PROPERTIES_FILE))
+end
 
 def log_with_timing(section_name)
   start_time = Time.now
-  Rails.logger.info "\n--- Starting #{section_name} at #{start_time} ---"
-  yield
+  puts "\n--- Starting #{section_name} at #{start_time} ---"
+  value = yield
   end_time = Time.now
-  Rails.logger.info "--- Finished #{section_name} at #{end_time}, took #{end_time - start_time} seconds ---\n"
+  puts "--- Finished #{section_name} at #{end_time}, took #{end_time - start_time} seconds ---\n"
+  value
 end
 
-def read_endpoints
+def get server, endpoint
+    HTTParty.get("#{server['url']}#{endpoint}",{
+      query: endpoint,
+      headers: server['headers']
+    })
+end
+
+def write_data filename, data
+  File.open(filename, 'w') do |file|
+    file.write(data)
+  end
+end
+
+def write_diff endpoint, diff
+  FileUtils.mkdir_p "results"
+  data = "#{endpoint}\n\n#{diff}\n"
+  write_data 'results/results.diff', data
+end
+
+def execute_against_urls endpoints, properties
+
+  endpoints.each do |endpoint|
+    control = get(properties['control'],endpoint)
+    experiment = get(properties['experiment'],endpoint)
+    experiment = "foo"
+    diff = Diffy::Diff.new(control, experiment)
+    if diff.to_s != ""
+      write_diff endpoint, diff
+    end
+  end
+
 end
 
 ## Main
 
-read_endpoints
-execute
+properties = read_properties
+endpoints = read_file(ENDPOINTS_FILE)
+execute_against_urls(endpoints, properties)
